0f3391c2448f1611e916d8ec59ecabe8d3ab9e36
munyeoll <munyeol08@gmail.com>
2022-07-14 13:34:32 +0900

    First commit

---
+++ src/main/java/com/board/anm/AcAppApplication.java
@@ -0,0 +1,13 @@
+package com.board.anm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/board/anm/AcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.anm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
